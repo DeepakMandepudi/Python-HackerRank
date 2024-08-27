b0d4f580e229bc4dc6d6d195ea98c8a23a68fa54
Deepak <mandepudideepak15@gmail.com>
2024-08-27 14:38:46 -0500

    Task03 Question and Solution

---
+++ Task03/solution.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+# first task to perform integer division, which will return integer value but display in float.
+print(a//b)
+
+# Perform normal division which will return float value by default.
+print(a/b)
